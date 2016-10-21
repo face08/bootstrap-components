12992c74a5f6c813d91e9dd21899468c3b860ae9
rnowley <nowley.r@gmail.com>
2016-10-21 14:13:41 +1300

    added new components refactored progress bar.

--- bootstrapCustomElements.js
+++ bootstrapCustomElements.js
@@ -1,3 +1,48 @@
+
+class Alert extends HTMLElement {
+
+  constructor() {
+    super();
+  }
+
+  createdCallback() {
+    var dismissable = this.getAttribute('dismissable') || '';
+    var providedClass = this.getAttribute('class') || '';
+    var classAttribute = `class="alert ${providedClass}${dismissable === 'true' ? ' alert-dismissable' : ''}"`;
+    var content = this.innerHTML;
+
+    var dismissableContent = '';
+
+    if (dismissable === 'true') {
+      dismissableContent =
+        `<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+           <span aria-hidden="true">&times</span>
+         </button>`
+    }
+
+    this.outerHTML =
+      `<div ${classAttribute} role="alert">
+        ${dismissableContent}
+        ${content}
+       </div>`
+  }
+}
+
+class AlertLink extends HTMLElement {
+
+  constructor() {
+    super();
+  }
+
+  createdCallback() {
+    var href = this.getAttribute('href') || '#';
+    var content = this.innerHTML;
+
+    this, outerHTML =
+      `<a href="${href}" class="alert-link">${content}</a>`
+  }
+}
+
 class Badge extends HTMLElement {
 
   constructor() {
@@ -74,6 +119,38 @@ class Divider extends HTMLElement {
 
 }
 
+class Navbar extends HTMLElement {
+
+  constructor() {
+    super();
+  }
+
+  createdCallback() {
+    var id = this.getAttribute('id') || '';
+    var href = this.getAttribute('href') || '#';
+    var brandText = this.getAttribute('brand-text') || '';
+    var content = this.innerHTML;
+
+    this.outerHTML =
+      `<nav class="navbar navbar-default">
+         <div class="container-fluid">
+           <div class="navbar-header">
+             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#{id}" aria-expanded="false">
+               <span class="sr-only">Toggle navigation</span>
+               <span class="icon-bar"></span>
+               <span class="icon-bar"></span>
+               <span class="icon-bar"></span>
+             </button>
+             <a class="navbar-brand" href="${href}">Brand</a>
+           </div>
+           <div class="collapse navbar-collapse" id="${id}">
+             ${content}
+           </div>
+         </div>
+       </nav>`;
+  }
+}
+
 class ProgressBar extends HTMLElement {
 
   constructor() {
@@ -84,12 +161,37 @@ class ProgressBar extends HTMLElement {
     var min = this.getAttribute('data-min') || '0';
     var max = this.getAttribute('data-max') || '0';
     var current = this.getAttribute('data-current') || '0';
+    var context = this.getAttribute('context') || '';
+    var striped = this.getAttribute('striped') || 'false';
+    var animate = this.getAttribute('animate') || 'false';
     var content = this.innerHTML;
+
+    var classAttribute = "progress-bar";
+
+    if (context !== '') {
+      classAttribute += ` progress-bar-${context}`;
+    }
+
+    if (striped === 'true') {
+      classAttribute += " progress-bar-striped";
+    }
+
+    if (animate === 'true') {
+      classAttribute += " active";
+    }
+
+    classAttribute = `class="${classAttribute}"`
+
+    var screenReaderText = `${current}% Complete`;
+
+    if (context !== '') {
+      screenReaderText += ` (${context})`;
+    }
 
     this.outerHTML =
       `<div class="progress">
-         <div class="progress-bar" role="progressbar" aria-valuenow="${current}" aria-valuemin="${min}" aria-valuemax="${max}" style="width: ${current}%;">
-           <span class="sr-only">${current}% Complete</span>
+         <div ${classAttribute} role="progressbar" aria-valuenow="${current}" aria-valuemin="${min}" aria-valuemax="${max}" style="width: ${current}%;">
+           <span class="sr-only">${screenReaderText}</span>
 ${content}
          </div>
        </div>`;
@@ -171,6 +273,7 @@ class Dropdown extends HTMLElement {
   }
 }
 
+document.registerElement('x-alert', Alert);
 document.registerElement('x-badge', Badge);
 document.registerElement('x-divider', Divider);
 document.registerElement('x-progress-bar', ProgressBar);
