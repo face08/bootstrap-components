e1a1d6658ceb6a0ca578619d57bb08cae6c2f6d5
nowley.r <nowley.r@gmail.com>
2016-10-21 23:40:44 +1300

    Navbar related refactoring.

--- bootstrapCustomElements.js
+++ bootstrapCustomElements.js
@@ -127,12 +127,21 @@ class Navbar extends HTMLElement {
 
   createdCallback() {
     var id = this.getAttribute('id') || '';
+    var position = this.getAttribute('position') || '';
     var href = this.getAttribute('href') || '#';
     var brandText = this.getAttribute('brand-text') || '';
     var content = this.innerHTML;
 
+    var classAttribute = "navbar navbar-default";
+
+    if (position !== '') {
+      classAttribute += ` navbar-${position}`;
+    }
+
+    classAttribute = `class="${classAttribute}"`
+
     this.outerHTML =
-      `<nav class="navbar navbar-default">
+      `<nav ${classAttribute}>
          <div class="container-fluid">
            <div class="navbar-header">
              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#${id}" aria-expanded="false">
@@ -195,7 +204,7 @@ class ProgressBar extends HTMLElement {
       classAttribute += " active";
     }
 
-    classAttribute = `class="${classAttribute}"`
+    classAttribute = `class="${classAttribute}"`;
 
     var screenReaderText = `${current}% Complete`;
 
@@ -214,7 +223,30 @@ ${content}
 
 }
 
-class MenuItem extends HTMLElement {
+class NavbarMenuItem extends HTMLElement {
+
+  constructor() {
+    super();
+  }
+
+  createdCallback() {
+    var href = this.getAttribute('href') || '#';
+    var active = this.getAttribute('active') || '';
+    var content = this.innerHTML;
+    var screenReaderText = '';
+    var classAttribute = '';
+
+    if (active !== '') {
+      screenReaderText = ' <span class="sr-only">(current)</span>';
+      classAttribute = ' class="active"';
+    }
+
+    this.outerHTML =
+      `<li${classAttribute}><a href="${href}">${content}${screenReaderText}</a></li>`;
+  }
+}
+
+class DropdownItem extends HTMLElement {
 
   constructor() {
     super();
@@ -245,6 +277,85 @@ class NavbarForm extends HTMLElement {
          </div>
          <button type="submit" class="btn btn-default">${buttonText}</button>
        </form>`;
+  }
+
+}
+
+class NavbarDropdown extends HTMLElement {
+
+  constructor() {
+    super();
+  }
+
+  createdCallback() {
+    var title = this.getAttribute('title') || '';
+    var content = this.innerHTML;
+
+    this.outerHTML =
+      `<ul class="nav navbar-nav">
+         <li class="dropdown">
+           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true
+             aria-expanded="false">${title} <span class="caret"></span></a>
+           <ul class="dropdown-menu">
+             ${content}
+           </ul>
+         </li>
+       </ul>`;
+  }
+
+}
+
+class NavbarButton extends HTMLElement {
+
+  constructor() {
+    super();
+  }
+
+  createdCallback() {
+    var content = this.innerHTML;
+
+    this.outerHTML =
+      `<button type="button" class="btn btn-default navbar-btn">${content}</button>`;
+  }
+
+}
+
+class NavbarText extends HTMLElement {
+
+  constructor() {
+    super();
+  }
+
+  createdCallback() {
+    var content = this.innerHTML;
+
+    this.outerHTML =
+      `<p class="navbar-text">${content}</p>`
+  }
+
+}
+
+class NavbarLink extends HTMLElement {
+
+  constructor() {
+    super();
+  }
+
+  createdCallback() {
+    var href = this.getAttribute('href') || '#';
+    var alignment = this.getAttribute('alignment') || '';
+    var linkText = this.getAttribute('link-text') || '';
+
+    var classAttribute = "navbar-text";
+
+    if (position !== '') {
+      classAttribute += ` navbar-${alignment}`;
+    }
+
+    classAttribute = `class="${classAttribute}"`;
+
+    this.outerHTML =
+      `<p ${classAttribute}>${content}<a href="${href}" class="navbar-link">${link-text}</a></p>`;
   }
 
 }
@@ -313,12 +424,17 @@ document.registerElement('x-alert', Alert);
 document.registerElement('x-badge', Badge);
 document.registerElement('x-divider', Divider);
 document.registerElement('x-progress-bar', ProgressBar);
-document.registerElement('x-menu-item', MenuItem);
+document.registerElement('x-nav-menu-item', NavbarMenuItem);
 document.registerElement('x-dropdown-header', DropDownHeader);
 document.registerElement('x-dropdown', Dropdown);
+document.registerElement('x-dropdown-item', DropdownItem);
 document.registerElement('x-dropup', Dropup);
 document.registerElement('x-button-dropdown', ButtonDropdown);
 document.registerElement('x-navbar', Navbar);
 document.registerElement('x-navbar-menu', NavbarMenu);
 document.registerElement('x-navbar-form', NavbarForm);
+document.registerElement('x-navbar-dropdown', NavbarDropdown);
+document.registerElement('x-navbar-button', NavbarButton);
+document.registerElement('x-navbar-text', NavbarText);
+document.registerElement('x-navbar-link', NavbarLink);
 document.registerElement('x-split-button-dropdown', SplitButtonDropdown);
