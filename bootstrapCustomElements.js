4e54422f8ec190ae952f1c6940a29cc46a2f9122
nowley.r <nowley.r@gmail.com>
2016-11-27 21:58:05 +1300

    Added extra components.

--- bootstrapCustomElements.js
+++ bootstrapCustomElements.js
@@ -137,6 +137,159 @@ class SplitButtonDropdown extends HTMLElement {
   }
 }
 
+class Media extends HTMLElement {
+
+  constructor() {
+    super();
+  }
+
+  createdCallback() {
+    var content = this.innerHTML;
+    this.outerHTML =
+      `<div class="media">
+        ${content}
+       </div>`;
+  }
+}
+
+class MediaBody extends HTMLElement {
+
+  constructor() {
+    super();
+  }
+
+  createdCallback() {
+    var content = this.innerHTML;
+    this.outerHTML =
+      `<div class="media-body">
+        ${content}
+       </div>`;
+  }
+}
+
+class MediaHeading1 extends HTMLElement {
+
+  constructor() {
+    super();
+  }
+
+  createdCallback() {
+    var content = this.innerHTML;
+
+    this.outerHTML =
+      `<h1 class="media">${content}</h1>`;
+  }
+}
+
+class MediaHeading2 extends HTMLElement {
+
+  constructor() {
+    super();
+  }
+
+  createdCallback() {
+    var content = this.innerHTML;
+    this.outerHTML =
+      `<h2 class="media">${content}</h2>`;
+  }
+}
+
+class MediaHeading3 extends HTMLElement {
+
+  constructor() {
+    super();
+  }
+
+  createdCallback() {
+    var content = this.innerHTML;
+    this.outerHTML =
+      `<h3 class="media">${content}</h3>`;
+  }
+}
+
+class MediaHeading4 extends HTMLElement {
+
+  constructor() {
+    super();
+  }
+
+  createdCallback() {
+    var content = this.innerHTML;
+    this.outerHTML =
+      `<h4 class="media">${content}</h4>`;
+  }
+}
+
+class MediaHeading5 extends HTMLElement {
+
+  constructor() {
+    super();
+  }
+
+  createdCallback() {
+    var content = this.innerHTML;
+    this.outerHTML =
+      `<h5 class="media">${content}</h5>`;
+  }
+}
+
+class MediaHeading6 extends HTMLElement {
+
+  constructor() {
+    super();
+  }
+
+  createdCallback() {
+    var content = this.innerHTML;
+    this.outerHTML =
+      `<h6 class="media">${content}</h6>`;
+  }
+}
+
+class MediaObject extends HTMLElement {
+
+  constructor() {
+    super();
+  }
+
+  createdCallback() {
+    var alignment = this.getAttribute('alignment') || '';
+    var horizontalPosition = this.getAttribute('horizontal-position') || 'left';
+    var href = this.getAttribute('href') || '#';
+    var src = this.getAttribute('src') || '';
+    var alt = this.getAttribute('alt') || '';
+
+    var classAttribute = 'media-left ';
+
+    if (horizontalPosition === 'right') {
+      classAttribute = 'media-right ';
+    }
+
+    switch(alignment) {
+      case 'top':
+        classAttribute += 'media-top ';
+        break;
+      case 'middle':
+        classAttribute += 'media-middle ';
+        break;
+      case 'bottom':
+        classAttribute += 'media-bottom ';
+        break;
+    }
+
+    classAttribute = ` class="${classAttribute.trim()}"`;
+
+    var content = this.innerHTML;
+
+    this.outerHTML =
+      `<div${classAttribute}>
+        <a href="${href}">
+          <img class="media-object" src="${src}" alt="${alt}">
+        </a>
+       </div>`;
+  }
+}
+
 class Divider extends HTMLElement {
 
   constructor() {
@@ -146,6 +299,23 @@ class Divider extends HTMLElement {
   createdCallback() {
     this.outerHTML =
       `<li role="separator" class="divider"></li>`;
+  }
+
+}
+
+class Container extends HTMLElement {
+
+  constructor() {
+    super();
+  }
+
+  createdCallback() {
+    var content = this.innerHTML;
+
+    this.outerHTML =
+      `<div class="container">
+        ${content}
+      </div>`
   }
 
 }
@@ -176,7 +346,7 @@ class Navbar extends HTMLElement {
          <div class="container-fluid">
            <div class="navbar-header">
              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#${id}" aria-expanded="false">
-               <span class="sr-only">Toggle navigation</span>
+               <span class="sr-only">Toggle navigation</span>1
                <span class="icon-bar"></span>
                <span class="icon-bar"></span>
                <span class="icon-bar"></span>
@@ -240,14 +410,14 @@ class ProgressBar extends HTMLElement {
     var screenReaderText = `${current}% Complete`;
 
     if (context !== '') {
-      screenReaderText += ` (${context})`;
+      screenReaderText += ` (${context})`;Unexpected
     }
 
     this.outerHTML =
       `<div class="progress">
          <div ${classAttribute} role="progressbar" aria-valuenow="${current}" aria-valuemin="${min}" aria-valuemax="${max}" style="width: ${current}%;">
-           <span class="sr-only">${screenReaderText}</span>
-${content}
+          <span class="sr-only">${screenReaderText}</span>
+          ${content}
          </div>
        </div>`;
   }
@@ -324,7 +494,7 @@ class NavPill extends HTMLElement {
     }
 
     if (disabled === 'true') {
-      classAttribute = ' disabled';
+      classAttribute += ' disabled';
     }
 
     classAttribute = ` class="${classAttribute.trim()}"`;
@@ -366,7 +536,7 @@ class NavbarTabs extends HTMLElement {
     super();
   }
 
-  createdCallback() {
+  createdCallback() {1
     var justified = this.getAttribute('justified') || 'false';
     var content = this.innerHTML;
 
@@ -679,33 +849,391 @@ class Dropdown extends HTMLElement {
   }
 }
 
+class Jumbotron extends HTMLElement {
+
+  constructor() {
+    super();
+  }
+
+  createdCallback() {
+    var content = this.innerHTML;
+
+    this.outerHTML =
+      `<div class="jumbotron">
+        ${content}
+      </div>`;
+  }
+}
+
+class Pager extends HTMLElement {
+
+  constructor() {
+    super();
+  }
+
+  createdCallback() {
+    var ariaLabel = this.getAttribute('aria-label') || '';
+    var content = this.innerHTML;
+
+    this.outerHTML =
+      `<nav aria-label="${ariaLabel}">
+        <ul class="pager">
+          ${content}
+        </ul>
+      </nav>`;
+  }
+}
+
+class PagerPrevious extends HTMLElement {
+
+  constructor() {
+    super();
+  }
+
+  createdCallback() {
+    var sideAlign = this.getAttribute('side-align') || '';
+    var showArrow = this.getAttribute('show-arrow') || '';
+    var href = this.getAttribute('href') || '#';
+    var disabled = this.getAttribute('disabled') || '';
+    var content = this.innerHTML;
+    var showArrowContent = '';
+
+    if (showArrow === 'true') {
+      showArrowContent = '<span aria-hidden="true">&larr;</span>'
+    }
+
+    var classAttribute = '';
+
+    if (sideAlign === 'true') {
+      classAttribute = 'previous ';
+    }
+
+    if (disabled === 'true') {
+      classAttribute += 'disabled ';
+    }
+
+    classAttribute = ` class="${classAttribute.trim()}"`;
+
+    this.outerHTML =
+      `<li${classAttribute}>
+        <a href="${href}">${showArrowContent}${content}</a>
+      </li>`;
+  }
+}
+
+class PagerNext extends HTMLElement {
+
+  constructor() {
+    super();
+  }
+
+  createdCallback() {
+    var sideAlign = this.getAttribute('side-align') || '';
+    var showArrow = this.getAttribute('show-arrow') || '';
+    var href = this.getAttribute('href') || '#';
+    var disabled = this.getAttribute('disabled') || '';
+    var content = this.innerHTML;
+    var showArrowContent = '';
+
+    if (showArrow === 'true') {
+      showArrowContent = '<span aria-hidden="true">&rarr;</span>'
+    }
+
+    var classAttribute = '';
+
+    if (sideAlign === 'true') {
+      classAttribute = 'next ';
+    }
+
+    if (disabled === 'true') {
+      classAttribute += 'disabled ';
+    }
+
+    classAttribute = ` class="${classAttribute.trim()}"`;
+
+    this.outerHTML =
+      `<li${classAttribute}>
+        <a href="${href}">${content}${showArrowContent}</a>
+      </li>`;
+  }
+}
+
+class Panel extends HTMLElement {
+
+  constructor() {
+    super();
+  }
+
+  createdCallback() {
+    var panelType = this.getAttribute('panel-type') || '';
+    var content = this.innerHTML;
+
+    var classAttribute = "panel";
+
+    switch(panelType) {
+      case "primary":
+      classAttribute = `${classAttribute} panel-primary`;
+        break;
+      case "success":
+      classAttribute = `${classAttribute} panel-success`;
+        break;
+      case "info":
+      classAttribute = `${classAttribute} panel-info`;
+        break;
+      case "warning":
+      classAttribute = `${classAttribute} panel-warning`;
+        break;
+      case "danger":
+      classAttribute = `${classAttribute} panel-danger`;
+        break;
+      default:
+        classAttribute = `${classAttribute} panel-default`;
+        break;
+    }
+
+    classAttribute = `class="${classAttribute}"`;
+
+    this.outerHTML =
+      `<div ${classAttribute}>
+        ${content}
+      </div>`
+  }
+
+}
+
+class PanelBody extends HTMLElement {
+
+  constructor() {
+    super();
+  }
+
+  createdCallback() {
+    var content = this.innerHTML;
+
+    this.outerHTML =
+      `<div class="panel-body">
+        ${content}
+      </div>`
+  }
+}
+
+class PanelFooter extends HTMLElement {
+
+  constructor() {
+    super();
+  }
+
+  createdCallback() {
+    var content = this.innerHTML;
+
+    this.outerHTML =
+      `<div class="panel-footer">
+        ${content}
+      </div>`
+  }
+}
+
+class PanelHeading extends HTMLElement {
+
+  constructor() {
+    super();
+  }
+
+  createdCallback() {
+    var content = this.innerHTML;
+
+    this.outerHTML =
+      `<div class="panel-heading">
+        ${content}
+      </div>`
+  }
+}
+
+class PanelTitle1 extends HTMLElement {
+
+  constructor() {
+    super();
+  }
+
+  createdCallback() {
+    var content = this.innerHTML;
+
+    this.outerHTML =
+      `<h1 class="panel-title">
+        ${content}
+      </h1>`
+  }
+}
+
+class PanelTitle2 extends HTMLElement {
+
+  constructor() {
+    super();
+  }
+
+  createdCallback() {
+    var content = this.innerHTML;
+
+    this.outerHTML =
+      `<h2 class="panel-title">
+        ${content}
+      </h2>`
+  }
+}
+
+class PanelTitle3 extends HTMLElement {
+
+  constructor() {
+    super();
+  }
+
+  createdCallback() {
+    var content = this.innerHTML;
+
+    this.outerHTML =
+      `<h3 class="panel-title">
+        ${content}
+      </h3>`
+  }
+}
+
+class PanelTitle4 extends HTMLElement {
+
+  constructor() {
+    super();
+  }
+
+  createdCallback() {
+    var content = this.innerHTML;
+
+    this.outerHTML =
+      `<h4 class="panel-title">
+        ${content}
+      </h4>`
+  }
+}
+
+class PanelTitle5 extends HTMLElement {
+
+  constructor() {
+    super();
+  }
+
+  createdCallback() {
+    var content = this.innerHTML;
+
+    this.outerHTML =
+      `<h5 class="panel-title">
+        ${content}
+      </h5>`
+  }
+}
+
+class PanelTitle6 extends HTMLElement {
+
+  constructor() {
+    super();
+  }
+
+  createdCallback() {
+    var content = this.innerHTML;
+
+    this.outerHTML =
+      `<h6 class="panel-title">
+        ${content}
+      </h6>`
+  }
+}
+
+class Thumbnail extends HTMLElement {
+
+  constructor() {
+    super();
+  }
+
+  createdCallback() {
+    var href = this.getAttribute('href') || '#';
+    var src = this.getAttribute('src') || '';
+    var alt = this.getAttribute('alt') || '';
+
+    this.outerHTML =
+      `<a href="${href}" class="thumbnail">
+        <img src="${src}" alt="${alt}">
+      </a>`
+  }
+}
+
+class Well extends HTMLElement {
+
+  constructor() {
+    super();
+  }
+
+  createdCallback() {
+    var wellSize = this.getAttribute('well-size') || '';
+    var content = this.innerHTML;
+
+    var classAttribute = "well";
+
+    if (wellSize === 'small') {
+      classAttribute = `${classAttribute} well-sm`;
+    } else if (wellSize === 'large') {
+      classAttribute = `${classAttribute} well-lg`;
+    }
+
+    classAttribute = `class="${classAttribute}"`;
+
+    this.outerHTML =
+      `<div ${classAttribute}>
+        ${content}
+      </div>`;
+  }
+}
+
 document.registerElement('x-alert', Alert);
 document.registerElement('x-badge', Badge);
 document.registerElement('x-breadcrumbs', BreadCrumbs);
 document.registerElement('x-breadcrumb', BreadCrumb);
-document.registerElement('x-divider', Divider);
-document.registerElement('x-progress-bar', ProgressBar);
-document.registerElement('x-nav-menu-item', NavbarMenuItem);
-document.registerElement('x-dropdown-header', DropDownHeader);
-document.registerElement('x-dropdown', Dropdown);
-document.registerElement('x-dropdown-item', DropdownItem);
-document.registerElement('x-dropup', Dropup);
 document.registerElement('x-button-dropdown', ButtonDropdown);
-document.registerElement('x-link-list-group', LinkListGroup);
-document.registerElement('x-link-list-group-item', LinkListGroupItem);
-document.registerElement('x-list-group', ListGroup);
-document.registerElement('x-list-group-item', ListGroupItem);
+document.registerElement('x-container', Container);
+document.registerElement('x-divider', Divider);
+document.registerElement('x-media', Media);
+document.registerElement('x-media-body', MediaBody);
+document.registerElement('x-media-header1', MediaHeading1);
+document.registerElement('x-media-header2', MediaHeading2);
+document.registerElement('x-media-header3', MediaHeading3);
+document.registerElement('x-media-header4', MediaHeading4);
+document.registerElement('x-media-header5', MediaHeading5);
+document.registerElement('x-media-header6', MediaHeading6);
+document.registerElement('x-media-object', MediaObject);
+document.registerElement('x-nav-pill', NavPill);
+document.registerElement('x-nav-pill-dropdown', NavPillDropdown);
 document.registerElement('x-navbar', Navbar);
 document.registerElement('x-navbar-menu', NavbarMenu);
 document.registerElement('x-navbar-form', NavbarForm);
 document.registerElement('x-navbar-dropdown', NavbarDropdown);
 document.registerElement('x-navbar-button', NavbarButton);
+document.registerElement('x-navbar-tabs', NavbarTabs);
 document.registerElement('x-navbar-text', NavbarText);
 document.registerElement('x-navbar-link', NavbarLink);
+document.registerElement('x-nav-menu-item', NavbarMenuItem);
 document.registerElement('x-navbar-pills', NavbarPills);
-document.registerElement('x-nav-pill', NavPill);
-document.registerElement('x-nav-pill-dropdown', NavPillDropdown);
-document.registerElement('x-navbar-tabs', NavbarTabs);
 document.registerElement('x-nav-tab', NavTab);
 document.registerElement('x-nav-tab-dropdown', NavTabDropdown);
+document.registerElement('x-pager', Pager);
+document.registerElement('x-pager-next', PagerNext);
+document.registerElement('x-pager-previous', PagerPrevious);
+document.registerElement('x-panel', Panel);
+document.registerElement('x-panel-body', PanelBody);
+document.registerElement('x-panel-footer', PanelFooter);
+document.registerElement('x-panel-heading', PanelHeading);
+document.registerElement('x-panel-title-1', PanelTitle1);
+document.registerElement('x-panel-title-2', PanelTitle2);
+document.registerElement('x-panel-title-3', PanelTitle3);
+document.registerElement('x-panel-title-4', PanelTitle4);
+document.registerElement('x-panel-title-5', PanelTitle5);
+document.registerElement('x-panel-title-6', PanelTitle6);
+document.registerElement('x-progress-bar', ProgressBar);
 document.registerElement('x-split-button-dropdown', SplitButtonDropdown);
+document.registerElement('x-thumbnail', Thumbnail);
+document.registerElement('x-well', Well);
